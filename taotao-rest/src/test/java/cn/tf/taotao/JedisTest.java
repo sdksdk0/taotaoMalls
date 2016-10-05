98d83a20909b36840f1c6751ec420d0fed921d72
sdksdk0 <690644511@qq.com>
2016-10-05 23:38:16 +0800

    测试了redis集群环境

---
+++ taotao-rest/src/test/java/cn/tf/taotao/JedisTest.java
@@ -0,0 +1,61 @@
+package cn.tf.taotao;
+
+import java.util.HashSet;
+
+import org.junit.Test;
+
+import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisCluster;
+import redis.clients.jedis.JedisPool;
+
+public class JedisTest {
+	
+	//单机
+	@Test
+	public void tets1(){
+		//创建一个jedis的对象
+		//调用jedis对象的方法，方法名称和redis的命令一样
+		Jedis jedis=new Jedis("115.28.16.234",6379);
+		jedis.set("key1", "jedis test");
+		String string=jedis.get("key1");
+		System.out.println(string);
+		jedis.close();
+	}
+	
+	//使用连接池
+	@Test
+	public void test2(){
+		//创建jedis连接池
+		JedisPool pool=new JedisPool("115.28.16.234",6379);
+		//从连接池中获得jedis对象
+		Jedis jedis=pool.getResource();
+		String string=jedis.get("key1");
+		System.out.println(string);
+		jedis.close();
+	}
+	
+	
+	//集群
+	@Test
+	public void tese3(){
+		HashSet<HostAndPort> nodeSet=new HashSet<>();
+		nodeSet.add(new HostAndPort("115.28.16.234",7001));
+		nodeSet.add(new HostAndPort("115.28.16.234",7002));
+		nodeSet.add(new HostAndPort("115.28.16.234",7003));
+		nodeSet.add(new HostAndPort("115.28.16.234",7004));
+		nodeSet.add(new HostAndPort("115.28.16.234",7005));
+		nodeSet.add(new HostAndPort("115.28.16.234",7006));
+		
+		JedisCluster  cluster=new JedisCluster(nodeSet);
+		cluster.set("key1", "1000");
+		String string=cluster.get("key1");
+		System.out.println(string);
+		cluster.close();
+		
+		
+	}
+	
+	
+
+}
