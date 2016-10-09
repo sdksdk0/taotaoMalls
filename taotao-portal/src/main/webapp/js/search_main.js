d7e877a8b6db806578272299ece4883ee01c518f
sdksdk0 <690644511@qq.com>
2016-10-09 19:30:51 +0800

    fix bug

--- taotao-portal/src/main/webapp/js/search_main.js
+++ taotao-portal/src/main/webapp/js/search_main.js
@@ -1286,7 +1286,7 @@ SEARCH.page = function(b, a) {
     if (b < 1) {
         b = 1
     }
-    window.location = "http://localhost:8082/search.html?q="+SEARCH.query+"&page="+b;
+    window.location = "http://localhost:8085/search.html?q="+SEARCH.query+"&page="+b;
 //    if (a) {
 //        window.scrollTo(0, $("#filter").offset().top)
 //    }
